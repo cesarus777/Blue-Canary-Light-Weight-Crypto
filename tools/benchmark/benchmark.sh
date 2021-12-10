070efc8752cff615bbb1819a0fd068d959c534d2
cesarus777 <ulius.cesarus@gmail.com>
2021-12-10 10:40:24 +0300

    Improve logging readability

--- tools/benchmark/benchmark.sh
+++ tools/benchmark/benchmark.sh
@@ -2,12 +2,15 @@
 
 src_dir="$(readlink -f $(dirname $0)/../..)"
 benchmark_log="${src_dir}/benchmark.log"
+echo "" >> ${benchmark_log}
 
+  echo "$(date)" >> ${benchmark_log}
 for cc in "gcc" "clang"; do
   ${src_dir}/tools/install.sh -c "${cc}" || exit $?
-  echo "$(date) ${cc}" >> ${benchmark_log}
+  echo "${cc}" >> ${benchmark_log}
   for benchmark in ${src_dir}/install/benchmark/*; do
     benchmark_name="$(basename ${benchmark})"
+    echo "Running ${benchmark_name}-${cc}"
     echo -n "${benchmark_name} " >> ${benchmark_log}
     ${benchmark} >> ${benchmark_log}
   done
